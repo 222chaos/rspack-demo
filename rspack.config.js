add36e54925cd72677d0ed4b3b321428ce1b17f8
2Chaos <ace.wonder@qq.com>
2023-04-04 23:23:20 +0800

    增加若干组件

--- rspack.config.js
+++ rspack.config.js
@@ -8,4 +8,25 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        //
+        // Match js, jsx, ts & tsx files
+        test: /\.[jt]sx?$/,
+        loader: "esbuild-loader",
+        options: {
+          // JavaScript version to compile to
+          target: "es2015",
+        },
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
